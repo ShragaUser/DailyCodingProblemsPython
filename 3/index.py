f9058a15bca7c84b05e6eec54647cec78ce2a7ae
ShragaUser <dandankidon@gmail.com>
2018-12-28 21:50:05 +0200

    started work on 3rd challenge

---
+++ 3/index.py
@@ -0,0 +1,11 @@
+from node import Node
+
+def main():
+    node = Node('root')
+    print "Starting challenge 3:\n"
+    serialized = serialize(node)
+    print serialized 
+
+def serialize(node):
+        
+
---
+++ 3/node.py
@@ -0,0 +1,7 @@
+#Node Class Defenition:
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
